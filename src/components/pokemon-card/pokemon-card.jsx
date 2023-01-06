f8abde5223b9b9638bd63c58c8e4854df7d30711
sarahmjenkins <sarahmjenkins88@gmail.com>
2023-01-06 16:53:25 -0700

    Changed to functional component, added modal

--- src/components/pokemon-card/pokemon-card.jsx
+++ src/components/pokemon-card/pokemon-card.jsx
@@ -1,69 +1,78 @@
-import React from 'react';
+// Attempt to use hooks
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Modal } from 'react-bootstrap';
 
-export class PokemonCard extends React.Component {
+export function PokemonCard(pokemon) {
+  const [showDetails, setShowDetails] = useState(false);
+  const [pokemonImage, setPokemonImage] = useState('');
+  const [pokemonHeight, setPokemonHeight] = useState('');
+  const [pokemonTypes, setPokemonTypes] = useState([]);
 
-  constructor() {
-    super();
-    this.state = {
-      showDetails: false,
-      pokemonImage: '',
-      pokemonHeight: '',
-      pokemonTypes: [],
-    };
-  }
+  const handleClose = () => setShowDetails(false);
+  const handleShow = () => setShowDetails(true);
 
-  handleClick = () => {
-    this.setState({
-      showDetails: !this.state.showDetails
-    })
-  }
+  // useEffect(() => {
+  //   fetch('props.url')
+  //   .then(response => {
+  //     setPokemonImage(response.data.sprites.front_default);
+  //     setPokemonHeight(response.data.height);
+  //     setPokemonTypes(response.data.types);
+  //   })
+  //   .catch(error => {
+  //     console.log(error);
+  //   });
+  // }, []);
 
-  getPokemonDetails = () => {
-    const { pokemon } = this.props;
-    const detailsUrl = pokemon.url;
+  // handleClick = () => {
+  //   setShowDetails(!showDetails)
+  // }
 
-    axios.get(detailsUrl)
-    .then(response => {
-      this.setState({
-        pokemonImage: response.data.sprites.front_default,
-        pokemonHeight: response.data.height,
-        pokemonTypes: response.data.types,
-      })
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  }
+  // getPokemonDetails = () => {
+  //   const { pokemon } = props;
+  //   const detailsUrl = pokemon.url;
 
+  //   axios.get(detailsUrl)
+  //   .then(response => {
+  //     setPokemonImage(response.data.sprites.front_default);
+  //     setPokemonHeight(response.data.height);
+  //     setPokemonTypes(response.data.types);
+  //   })
+  //   .catch(error => {
+  //     console.log(error);
+  //   });
+  // }
 
-  render() {
-    const { pokemon } = this.props;
-    const { pokemonImage, pokemonHeight, pokemonTypes } = this.state;
-
-    return (
-  
-      <Card className="pokemon-card text-center" onClick={this.getPokemonDetails}>
+  return(
+    <>
+      <Card className="pokemon-card text-center" onClick={handleShow}>
         <Card.Title className="text-center">
-          {pokemon.name.charAt(0).toUpperCase()+pokemon.name.slice(1)}
+          Pokemon's name goes here
+          {/* {pokemon.name} */}
+          {/* {pokemon.name.charAt(0).toUpperCase()+pokemon.name.slice(1)} */}
         </Card.Title>
-      
-      <div className="pokemon-details">
-        {this.state.showDetails && 
-          <Card.Body>
-            <Card.Img crossOrigin="anonymous" src={pokemonImage}/>
-            <p><b>Height</b>: {pokemonHeight} meters</p>
-            <p><b>Types</b>: {pokemonTypes.map((item) => item.type.name).join(', ') }</p>
-          </Card.Body>
-        }
-        {this.state.showDetails
-          ? <Button className="toggle-button" variant="outline-primary"  size="sm" onClick={this.handleClick}>Hide Details</Button>
-          : <Button className="toggle-button" variant="outline-primary" size="sm" onClick={this.handleClick}>Show Details</Button>
-        }
-        
-      </div>
       </Card>
-    )
-  }
+
+      <Modal
+        show={showDetails}
+        onHide={handleClose}
+        size="lg"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Pokemon's name will go here
+            {/* {props.name.charAt(0).toUpperCase()+props.name.slice(1)} */}
+          </Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          Pokemon's image, height, and types will go here.
+          {/* <Image crossOrigin="anonymous" src={pokemonImage}/>
+          <p><b>Height</b>: {pokemonHeight} meters</p>
+          <p><b>Types</b>: {pokemonTypes.map((item) => item.type.name).join(', ') }</p> */}
+        </Modal.Body>
+      </Modal>
+    </>
+  )
 }
