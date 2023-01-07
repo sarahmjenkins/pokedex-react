9628d9abd45c61f7b17e3ae4faee27f21fd7c453
sarahmjenkins <sarahmjenkins88@gmail.com>
2023-01-07 11:05:03 -0700

    Changed MainView to functional component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,37 +1,19 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { PokemonCard } from '../pokemon-card/pokemon-card';
 
-export class MainView extends React.Component {
+export function MainView() {
 
-  constructor() {
-    super();
-    this.state = {
-      pokemonList: [],
-    };
-  }
+  const [pokemonList, setPokemonList] = useState([]);
 
-  componentDidMount() {
-    this.getPokemon();
-  }
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=150')
+    .then(response => response.json())
+    .then(response => setPokemonList(response.results))
+    .catch(error => console.log(error))
+  })
 
-  getPokemon() {
-    axios.get('https://pokeapi.co/api/v2/pokemon/?limit=150')
-    .then(response => {
-      this.setState({
-        pokemonList: response.data.results
-      });
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  }
-
-  render() {
-    const { pokemonList } = this.state;
-
-    return (
+  return(
       <Container>
         <h1 className="text-center">Sarah's Pokedex</h1>
         <Row>
@@ -42,35 +24,5 @@ export class MainView extends React.Component {
           ))}
         </Row>
       </Container>
-    )
-  }
+  )
 }
-
-// import React, { useState, useEffect } from 'react';
-// import { Container, Row, Col } from 'react-bootstrap';
-// import { PokemonCard } from '../pokemon-card/pokemon-card';
-
-// export function MainView() {
-
-//   const [pokemonList, setPokemonList] = useState([]);
-
-//   useEffect(() => {
-//     fetch('https://pokeapi.co/api/v2/pokemon/?limit=150')
-//     .then(response => response.json)
-//     .then(response => setPokemonList(response.results))
-//     .catch(error => console.log(error))
-//   })
-
-//   return(
-//       <Container>
-//         <h1 className="text-center">Sarah's Pokedex</h1>
-//         <Row>
-//           {{pokemonList}.map((p) => (
-//             <Col xs={6} sm={5} md={4} lg={3} key={p.name}>
-//               <PokemonCard pokemon={p} />
-//             </Col>
-//           ))}
-//         </Row>
-//       </Container>
-//   )
-// }
