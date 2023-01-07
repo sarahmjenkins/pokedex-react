d431137c607b7f0731f29de6718cf7f960d8ce81
sarahmjenkins <sarahmjenkins88@gmail.com>
2023-01-06 17:41:51 -0700

    Tried changing main view to functional component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -22,7 +22,6 @@ export class MainView extends React.Component {
       this.setState({
         pokemonList: response.data.results
       });
-      console.log(response.data)
     })
     .catch(error => {
       console.log(error);
@@ -46,3 +45,32 @@ export class MainView extends React.Component {
     )
   }
 }
+
+// import React, { useState, useEffect } from 'react';
+// import { Container, Row, Col } from 'react-bootstrap';
+// import { PokemonCard } from '../pokemon-card/pokemon-card';
+
+// export function MainView() {
+
+//   const [pokemonList, setPokemonList] = useState([]);
+
+//   useEffect(() => {
+//     fetch('https://pokeapi.co/api/v2/pokemon/?limit=150')
+//     .then(response => response.json)
+//     .then(response => setPokemonList(response.results))
+//     .catch(error => console.log(error))
+//   })
+
+//   return(
+//       <Container>
+//         <h1 className="text-center">Sarah's Pokedex</h1>
+//         <Row>
+//           {{pokemonList}.map((p) => (
+//             <Col xs={6} sm={5} md={4} lg={3} key={p.name}>
+//               <PokemonCard pokemon={p} />
+//             </Col>
+//           ))}
+//         </Row>
+//       </Container>
+//   )
+// }
